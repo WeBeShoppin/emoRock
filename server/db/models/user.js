9cffa22e773a5bdb9dd1ee02ac4046eedf74b40e
Kristi Nanco <kristiknanco@gmail.com>
2020-08-04 15:02:01 -0400

    Add user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,6 +3,17 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING
+    // some people only have one name eg. Kali, Cher, Madonna
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
@@ -23,6 +34,19 @@ const User = db.define('user', {
     get() {
       return () => this.getDataValue('salt')
     }
+  },
+  address: {
+    type: Sequelize.STRING
+  },
+  phone: {
+    type: Sequelize.INTEGER
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defualt: false
+  },
+  cartStatus: {
+    type: Sequelize.STRING
   },
   googleId: {
     type: Sequelize.STRING
