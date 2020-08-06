cd4ca30798d9a80162a1c8ecf922122406327bfd
Kristi Nanco <kristiknanco@gmail.com>
2020-08-06 12:43:46 -0400

    Chore: Add columns to models and seed files

---
+++ server/db/models/cart.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Cart = db.define('cart', {
+  quantity: Sequelize.INTEGER,
+  purchasePrice: Sequelize.INTEGER
+})
+
+module.exports = Cart
