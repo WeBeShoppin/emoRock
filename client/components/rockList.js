708e4a2b39ccc8fcd6e2391603b33f9f07d246c8
Kristi Nanco <kristiknanco@gmail.com>
2020-08-10 15:15:56 -0400

    Refactor: Removed unused variable from rockList

--- client/components/rockList.js
+++ client/components/rockList.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {getAllRocks} from '../store/rocks'
 import {useDispatch, useSelector} from 'react-redux'
 import SingleRock from './singleRock'
