7b04d03199515e700da991975ae592b970949022
Slowgen <kalibobailey@gmail.com>
2020-08-11 15:33:56 -0400

    Feat: indvidual cart under userID api route

--- server/api/users.js
+++ server/api/users.js
@@ -35,6 +35,22 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+router.get('/:userId/cart', async (req, res, next) => {
+  try {
+    if (req.user.id === req.params.userId || req.user.isAdmin) {
+      const user = await User.findByPk(req.params.userId)
+
+      if (user) {
+        res.json(user)
+      }
+    } else {
+      res.status(404).send('The user is not found')
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
 router.get('/:userId', async (req, res, next) => {
   try {
     if (req.user.id === req.params.userId || req.user.isAdmin) {
