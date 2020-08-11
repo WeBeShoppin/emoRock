9e29a09cad0e92f9381c4bb4651501aed6762b43
Slowgen <kalibobailey@gmail.com>
2020-08-11 15:42:01 -0400

    chore: clean up excess commented code

--- server/api/users.js
+++ server/api/users.js
@@ -2,22 +2,10 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-// const adminMiddleware = (req, res, next) => {
-//   if (!req.user || !req.user.isAdmin) {
-//     res.sendStatus(401)
-//     next(error)
-//   }
-// }
-
-// const isAdmin = req.user.isAdmin
-
 router.get('/', async (req, res, next) => {
   try {
     if (req.user.isAdmin) {
       const users = await User.findAll({
-        // explicitly select only the id and email fields - even though
-        // users' passwords are encrypted, it won't help if we just
-        // send everything to anyone who asks!
         attributes: [
           'id',
           'email',
