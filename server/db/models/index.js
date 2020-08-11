82405cf18f843de91ded2df7f253a3acc250199c
Kristi Nanco <kristiknanco@gmail.com>
2020-08-11 12:22:55 -0400

    Refactor: Cheaper rocks, added associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -21,14 +21,12 @@ User.hasMany(Order)
 User.hasOne(Cart)
 
 Cart.belongsTo(User)
+Cart.hasMany(Rock)
 
 Order.belongsTo(User)
 Order.hasMany(Rock)
 
 Rock.belongsToMany(Order, {through: Cart})
-//Order.hasOne(Cart)
-//Cart.hasMany(Rock)
-//Rock.belongsToMany(Cart)
 
 module.exports = {
   User,
