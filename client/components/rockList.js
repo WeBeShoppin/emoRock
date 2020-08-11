18ca00e183cec378c65ee40bfb381489495fbbc0
Deanna Dunne <deanna.dunne@gmail.com>
2020-08-11 16:06:15 -0400

    STYLE: made rock list pretty

--- client/components/rockList.js
+++ client/components/rockList.js
@@ -6,7 +6,35 @@ import {Link} from 'react-router-dom'
 import {addItemToLocalStorage} from '../store/cart'
 import {useToasts} from 'react-toast-notifications'
 
+//MATERIALIZE
+import {makeStyles} from '@material-ui/core/styles'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import GridListTileBar from '@material-ui/core/GridListTileBar'
+import ListSubheader from '@material-ui/core/ListSubheader'
+import IconButton from '@material-ui/core/IconButton'
+import AddShoppingCartIcon from '@material-ui/icons/Info'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: 1000,
+    height: 450
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)'
+  }
+}))
+
 function RockList() {
+  const classes = useStyles() //materialize
+
   const rocks = useSelector(state => state.rocks)
   const {addToast} = useToasts()
   const dispatch = useDispatch()
@@ -44,19 +72,44 @@ function RockList() {
 
   return (
     <div>
-      <div>
-        <label htmlFor="categoriesFilter">Shop by categories: </label>
-        <select onChange={handleSelectChange} name="categoriesFilter">
-          <option>All</option>
-          <option>Fear</option>
-          <option>Enjoyment</option>
-          <option>Anger</option>
-          <option>Sadness</option>
-          <option>Miscellaneous</option>
-        </select>
+      <div className={classes.root}>
+        <GridList cellHeight={180} className={classes.gridList} cols={4}>
+          <GridListTile key="Subheader" cols={4} style={{height: 'auto'}}>
+            <ListSubheader component="div">
+              <label htmlFor="categoriesFilter">Shop by categories: </label>
+              <select onChange={handleSelectChange} name="categoriesFilter">
+                <option>All</option>
+                <option>Fear</option>
+                <option>Enjoyment</option>
+                <option>Anger</option>
+                <option>Sadness</option>
+                <option>Miscellaneous</option>
+              </select>
+            </ListSubheader>
+          </GridListTile>
+          {filteredRocks.map(rock => (
+            <GridListTile key={rock.id} cols={1}>
+              <Link to={`/rocks/${rock.id}`} component={SingleRock}>
+                <img src={rock.imageUrl} alt={rock.name} width="100%" />
+              </Link>
+              <GridListTileBar
+                title={rock.name}
+                subtitle={<span>${rock.price / 100}</span>}
+                actionIcon={
+                  <IconButton
+                    aria-label="add to shopping cart"
+                    onClick={() => handleAddBtn(rock)}
+                  >
+                    <AddShoppingCartIcon />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
       </div>
 
-      <div className="rockList">
+      {/* <div className="rockList">
         {filteredRocks.map(rock => (
           <div className="eachRock" key={rock.id}>
             <Link to={`/rocks/${rock.id}`} component={SingleRock}>
@@ -68,7 +121,7 @@ function RockList() {
             </button>
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   )
 }
